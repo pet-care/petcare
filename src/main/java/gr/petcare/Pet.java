c8fdd755f4618f30bdada6ebecc40f1288e59558
nikolio <nikolio@post.cz>
2016-04-17 00:44:52 +0300

    added pet post method

--- src/main/java/gr/petcare/Pet.java
+++ src/main/java/gr/petcare/Pet.java
@@ -1,12 +1,22 @@
 package gr.petcare;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Created by user on 16/4/2016.
  */
 public class Pet {
+
+    @Size(max = 64)
     private String name;
+    @NotNull
+    @Size(max = 64)
     private String ContactName;
+    @NotNull
+    @Size(max = 64)
     private String ContactPhone;
+
     private String photo;
     public String getName() {
         return name;
--- src/main/java/gr/petcare/Service.java
+++ src/main/java/gr/petcare/Service.java
@@ -4,14 +4,20 @@ package gr.petcare;
  * Created by user on 16/4/2016.
  */
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Service {
     private String  version = "1.0";
-
+    private List<Pet> pets= new ArrayList<Pet>();
     @RequestMapping("/version")
     public String getVersion() {
         return version;
@@ -27,6 +33,14 @@ public class Service {
     @RequestMapping("/mock-pets")
     public List<Pet> getPets() {
         MockPetFactory petFactory = new MockPetFactory();
-        return  petFactory.getPets();
+        this.pets = petFactory.getPets();
+        return this.pets;
     }
+    @RequestMapping("/new/pet", method = RequestMethod.POST)
+    public ResponseEntity<Pet> createPet(@RequestBody @Valid final Pet pet) {
+        this.pets.add(pet);
+        //return pet;
+
+    }
+
 }
