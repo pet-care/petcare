a1c041e5216baf0651ca548c89b9e3104adb8795
nikolio <nikolio@post.cz>
2016-04-16 11:05:13 +0300

    added SpringBootServletInitializer

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+/**
+ * Created by user on 16/4/2016.
+ */
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+}
